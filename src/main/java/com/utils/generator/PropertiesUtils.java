e5e2ce1db690fad38680258024699369932f0e7c
ShuPF8 <517292069@qq.com>
2017-07-22 22:01:32 +0800

    单列模式读取properties文件

---
+++ src/main/java/com/utils/generator/PropertiesUtils.java
@@ -0,0 +1,53 @@
+package com.utils.generator;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/** 单列模式获取 properties 配置文件
+ * @Auther SPF
+ * @Create 2017/7/21
+ */
+public class PropertiesUtils {
+    private static PropertiesUtils pu;
+    private static Properties properties = new Properties();
+
+    /** 单列模式必须私有构造器
+     * 两种获取配置文件的方法
+     * 1. 通过获取文件路径再用 InputStream 读取配置文件
+     *   String path = URLDecoder.decode(PropertiesUtils.class.getResource("/jdbc.properties").getFile(),"UTF-8");
+     *   InputStream is = new FileInputStream(path);
+     * 2. 直接通过 PropertiesUtils.class.getClassLoader().getResourceAsStream("jdbc.properties"); 获取 InputStream 流
+     */
+    private PropertiesUtils(){
+        InputStream is = PropertiesUtils.class.getClassLoader().getResourceAsStream("jdbc.properties");
+        try {
+            properties.load(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static PropertiesUtils getInstance(){
+        if (pu == null) {
+            pu = new PropertiesUtils();
+        }
+        return pu;
+    }
+
+    public Object getString(String key) {
+        return properties.get(key);
+    }
+
+    public static void main(String[] args) {
+        PropertiesUtils propertiesUtils = PropertiesUtils.getInstance();
+        Object obj = propertiesUtils.getString("jdbc.url");
+        System.out.println(obj);
+    }
+
+}
